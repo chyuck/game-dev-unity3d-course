60d9a88dda0659f73bc1d91d8db4b28b1d2486f0
Andrey Sergeev <andrey@chyuck.com>
2019-02-09 23:12:07 -0500

    Added completed games

---
+++ Box Shooter/Assets/Scripts/InitialLevel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AssemblyCSharp
+{
+	public class InitialLevel : MonoBehaviour {
+
+		// respond on collisions
+		void OnCollisionEnter(Collision newCollision)
+		{
+			// only do stuff if hit by a projectile
+			if (newCollision.gameObject.tag == "Projectile") {
+				// call the NextLevel function in the game manager
+				GameManager.gm.InitialLevel();
+			}
+		}
+	}
+}
+
